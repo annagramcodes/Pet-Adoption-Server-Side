c99a429e0905fa8f581bb95de6448af10b4d88fb
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-13 17:08:02 +0100

    fixes create post

--- models/Animal.model.js
+++ models/Animal.model.js
@@ -6,7 +6,11 @@ const animalSchema = new Schema({
     color: String,
     species: String,
     breed: String,
-    imageUrl: String, 
+    imageUrl: {
+        type: String,
+        // default:
+        //   'https://res.cloudinary.com/mraferreira/image/upload/v1652347918/movie-project/mvc_67565_urfvwr.jpg',
+      },
 })
 
 const Animal = model("Animal", animalSchema);
--- routes/animal.routes.js
+++ routes/animal.routes.js
@@ -3,27 +3,32 @@ const Animal = require("../models/Animal.model");
 const mongoose = require("mongoose");
 
 
-router.get("/adoption-post/create", (req, res, nextt) => {
-res.render("animals/create-adoption-post.hbs")
+/////////////////////////////
+// CREATING ADOPTION POSTS //
+router.get("/adoption-post/create", (req, res, next) => {
+  res.render("animals/create-adoption-post.hbs")
 })
 
 router.post("/adoption-post/create", (req, res, next) => {
     const { name, age, color, species, breed, imageUrl } = req.body;
-
+    
     Animal.create({ name, age, color, species, breed, imageUrl })
         .then((animal) =>
-            console.log(animal))
-        // res.redirect(`/animals/${animal._id}`)
+            res.send('okay'))
+        .catch((err) => next(err))
+        // res.redirect(`/animals/${animal._id}`) 
 })
 
 
+//////////////////////////////////////////
+// RENDERING THE LIST OF ADOPTION POSTS //
 
-// router.get("/animals-for-adoption", (res, req, next) =>
-//   Animal.find({})
-//     .then((animals) => {
-//       res.render("animals/animal-list.hbs", { animals });
-//     })
-//     .catch((err) => next(err))
-// );
+router.get("/animals-for-adoption", (res, req, next) =>
+  Animal.find({species: cat})
+    .then((animal) => {
+      res.render("animals/animal-list.hbs", { animal });
+    })
+    .catch((err) => next(err))
+);
 
 module.exports = router;
