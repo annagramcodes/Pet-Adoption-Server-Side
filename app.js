5315abe5a72e5a37e7e358cbc842ac48f5f08cd2
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-13 16:20:12 +0100

    adds animal routes and corresponding hbs.files

--- app.js
+++ app.js
@@ -30,7 +30,11 @@ app.use("/", index);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const animalRoutes = require("./routes/animal.routes");
+app.use("/", animalRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
+
