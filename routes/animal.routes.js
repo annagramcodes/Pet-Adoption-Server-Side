79a3318eb47e13db2ec853686d547974448416fe
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-16 15:13:46 +0100

    adds delete animals

--- routes/animal.routes.js
+++ routes/animal.routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const Animal = require("../models/Animal.model");
 const mongoose = require("mongoose");
+
 const fileUploader = require("../config/cloudinary.config");
+const res = require("express/lib/response");
 
 /////////////////////////////
 // CREATING ADOPTION POSTS //
@@ -13,46 +15,55 @@ router.post(
   "/adoption-post/create",
   fileUploader.single("animal-img"),
   (req, res, next) => {
-    const { name, age, color, species, breed, character, info, imageUrl } = req.body;
+    const { name, age, color, species, breed, character, info, imageUrl } =
+      req.body;
 
-    Animal.create({ name, age, color, species, breed, character, info, imageUrl: req.file.path })
+    Animal.create({
+      name,
+      age,
+      color,
+      species,
+      breed,
+      character,
+      info,
+      imageUrl: req.file.path,
+    })
       .then((animal) => {
         res.redirect(`/adoption-post/${animal._id}`);
       })
       .catch((err) => next(err));
   }
 );
-/////////////////////////////
-// VIEWING ADOPTION POST ///
-
-router.get("/adoption-post/:id", (req, res, next) => {
-  const { id } = req.params;
-
-    Animal.findById(id).then((animal) => {
-        res.render("animals/animal-details", { animal })
-    }
-  );
-});
 
 ////////////////////////////////
 // EDITING ADOPTION POSTS ////
-
 router.get("/adoption-post/:id/edit", (req, res, next) => {
   const { id } = req.params;
-  Animal.findById(id).then((animal) => {
-    res.render('animals/edit-adoption-post', animal)
-  })
-  .catch((err) => next(err));
-})
+  Animal.findById(id)
+    .then((animal) => {
+      res.render("animals/edit-adoption-post", animal);
+    })
+    .catch((err) => next(err));
+});
 
 router.post(
   "/adoption-post/:id/edit",
   fileUploader.single("animal-img"),
   (req, res, next) => {
     const { id } = req.params;
-    const { name, age, color, species, breed, character, info, imageUrl } = req.body;
+    const { name, age, color, species, breed, character, info, imageUrl } =
+      req.body;
 
-    Animal.findByIdAndUpdate(id,{ name, age, color, species, breed, character, info, imageUrl: req.file.path })
+    Animal.findByIdAndUpdate(id, {
+      name,
+      age,
+      color,
+      species,
+      breed,
+      character,
+      info,
+      imageUrl: req.file.path,
+    })
       .then((animal) => {
         res.redirect(`/adoption-post/${animal._id}`);
       })
@@ -60,11 +71,28 @@ router.post(
   }
 );
 
-router.get('/adoption-post/{{animal._id}}/delete')
+/////////////////////////////
+// DELETING ADOPTION POST ///
+router.post("/adoption-post/:id/delete", (req, res, next) => {
+  const { id } = req.params;
+
+  Animal.findByIdAndRemove(id)
+    .then(() => res.redirect('/'))
+    .catch((err) => next(err));
+});
+
+/////////////////////////////
+// VIEWING ADOPTION POST ///
+router.get("/adoption-post/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  Animal.findById(id).then((animal) => {
+    res.render("animals/animal-details", { animal });
+  });
+});
 
 //////////////////////////////////////////
 // RENDERING THE LIST OF ADOPTION POSTS //
-
 router.get("/animals-for-adoption/cats", (req, res, next) => {
   Animal.find({ species: "cat" })
     .then((animal) => {
