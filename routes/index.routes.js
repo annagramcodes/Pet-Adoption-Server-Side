4942ac5403bf1a54922ac3aab91cff0a84f87e44
Duarte Alves <duartemalves12@gmail.com>
2022-05-18 17:50:29 +0100

    error pages

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,17 @@
+const express = require("express");
+const res = require("express/lib/response");
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  req.app.locals.user = null;
   res.render("index", { user: req.session.user });
 });
 
+router.get("/500", (req, res, next) => {
+  req.session.user;
+  req.app.locals.user = null;
+  res.render("500");
+});
 module.exports = router;
